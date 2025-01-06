420489ee8ba10eb3435a448619e5c9fe51fefa9b
Rob Godbey <rbgodbey@yahoo.com>
2025-01-06 15:36:33 -0500

    Wrap up with Daisy UI

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import AddToCart from "./AddToCart";
 /* import styles from "./ProductCard.module.css"; */
 
+/* className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500" */
 const ProductCard = () => {
   return (
-    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500">
+    <div>
       <AddToCart />
     </div>
   );
